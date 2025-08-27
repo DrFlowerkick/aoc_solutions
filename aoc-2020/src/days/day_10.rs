93b4cedd17d3e287caa9431903721a0eee1668a0
DrFlowerkick <flowerkick14@gmail.com>
2025-08-27 20:45:15 +0200

    Finished day 10 of aoc 2020.

--- aoc-2020/src/days/day_10.rs
+++ aoc-2020/src/days/day_10.rs
@@ -1,15 +1,16 @@
 //!day_10.rs
 
 use anyhow::Result;
+use std::collections::HashMap;
 
 struct ChallengeInput {
-    numbers: Vec<u64>,
+    adapters: Vec<u64>,
 }
 
 impl From<&str> for ChallengeInput {
     fn from(value: &str) -> Self {
         ChallengeInput {
-            numbers: value.lines().filter_map(|n| n.parse().ok()).collect(),
+            adapters: value.lines().filter_map(|n| n.parse().ok()).collect(),
         }
     }
 }
@@ -18,12 +19,12 @@ impl ChallengeInput {
     fn solution_part_1(&mut self) -> u64 {
         let mut count_dif_1 = 0;
         let mut count_dif_3 = 0;
-        self.numbers.push(0);
-        self.numbers.sort();
-        let max = *self.numbers.last().unwrap();
-        self.numbers.push(max + 3);
-        for (i, n) in self.numbers[..self.numbers.len() - 1].iter().enumerate() {
-            match self.numbers[i + 1] - *n {
+        self.adapters.push(0);
+        self.adapters.sort();
+        let max = *self.adapters.last().unwrap();
+        self.adapters.push(max + 3);
+        for (i, n) in self.adapters[..self.adapters.len() - 1].iter().enumerate() {
+            match self.adapters[i + 1] - *n {
                 3 => count_dif_3 += 1,
                 2 => (),
                 1 => count_dif_1 += 1,
@@ -33,7 +34,26 @@ impl ChallengeInput {
         count_dif_1 * count_dif_3
     }
     fn solution_part_2(&self) -> u64 {
-        0
+        let mut seen: HashMap<usize, u64> = HashMap::new();
+        self.count_adapters(0, &mut seen)
+    }
+    fn count_adapters(&self, adapter_index: usize, seen: &mut HashMap<usize, u64>) -> u64 {
+        if adapter_index == self.adapters.len() - 1 {
+            return 1;
+        }
+        if let Some(count) = seen.get(&adapter_index) {
+            return *count;
+        }
+        let adapter_jolts = self.adapters[adapter_index];
+        let mut count = 0;
+        for child_index in (adapter_index + 1..=adapter_index + 3)
+            .filter(|i| self.adapters.get(*i).is_some())
+            .filter(|i| self.adapters[*i] <= adapter_jolts + 3)
+        {
+            count += self.count_adapters(child_index, seen);
+        }
+        seen.insert(adapter_index, count);
+        count
     }
 }
 
@@ -47,7 +67,7 @@ pub fn solution() -> Result<()> {
 
     let result_part2 = challenge.solution_part_2();
     println!("result day_10 part 2: {result_part2}");
-    //assert_eq!(result_part2, YYY);
+    assert_eq!(result_part2, 43_406_276_662_336);
 
     Ok(())
 }
@@ -68,7 +88,7 @@ mod tests {
 
         let result_part2 = example.solution_part_2();
         println!("result day_10 part 2: {result_part2}");
-        //assert_eq!(result_part2, YYY);
+        assert_eq!(result_part2, 19_208);
 
         Ok(())
     }
